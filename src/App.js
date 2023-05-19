9466734917ef265368cdb8561a710c60d4cc57d2
leehyeryeong <w2108@e-mirim.hs.kr>
2023-05-19 12:10:33 +0900

    CSS Module

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import { Component } from "react";
-import SassComponent from "./SassComponent";
-
+import { Component } from 'react';
+import CSSModule from "./CSSModule";
 class App extends Component {
   render() {
     return (
       <div>
-        <SassComponent />
+        <CSSModule />
       </div>
     );
   }
