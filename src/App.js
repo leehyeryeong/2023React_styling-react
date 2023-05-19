f7b60dcf43823ba5d37f46de87358765ca07e561
leehyeryeong <w2108@e-mirim.hs.kr>
2023-05-19 12:19:16 +0900

    classnames

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import { Component } from 'react';
-import CSSModule from "./CSSModule";
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <CSSModule />
-      </div>
-    );
-  }
-}
+import styles from "./styles/CSSModule.module.css";
+import classNames from 'classnames/bind';
 
-export default App;
+const cx = classNames.bind(styles);
+
+const CSSModule = () => {
+  return (
+    <div className={cx("wrapper", "inverted")}>
+      안녕하세요, 저는 <span className="something">CSS Module!</span>
+      {/* CSSMoudle.module.css에서 :global로 클래스 이름을 설정하면 문자열 형태("something")으로 표시 */}
+    </div>
+  );
+};
+
+export default CSSModule;
